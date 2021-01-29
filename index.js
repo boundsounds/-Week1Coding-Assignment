8f141ca9585030f39dbe689c4b64ca54d73ac28a
boundsounds <76763933+boundsounds@users.noreply.github.com>
2021-01-29 12:48:16 -0700

    week 1 commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var itemPrice = 3.69;
+var moneyInWallet = 20.00;
+var numberOfFriends = 3;
+var ageInYears = 22;
+var firstName = ('Grant');
+var lastName = ('Hood');
+var middleInitial = ('M');
+
+
+
+console.log('The price of the item is: ' + itemPrice);
+console.log('The amount of money in my wallet is: ' + moneyInWallet);
+console.log('Amount of money left after purchase: ' + (moneyInWallet - itemPrice));
+console.log('The number of friends that I have is: ' + numberOfFriends);
+console.log('My current age is: ' + ageInYears);
+console.log('Average number of friends per year: ' + numberOfFriends / ageInYears);
+console.log('My full name is: ' + firstName + ' ' + middleInitial +' '+ lastName);
